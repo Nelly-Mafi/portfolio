2dbfadc4d987b259a10dba6c3ab01067c1788816
Nelly-Mafi <78750485+Nelly-Mafi@users.noreply.github.com>
2024-08-09 17:38:00 -0700

    Update script_wecare.js

--- js/script_wecare.js
+++ js/script_wecare.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Initialize Locomotive Scroll
-    const scroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
-    });
-
-    // Scroll down to the next section on click
     const scrollDownButton = document.getElementById('scroll-down');
     const bodySection = document.getElementById('body');
-    
-    scrollDownButton.addEventListener('click', function () {
-        scroll.scrollTo(bodySection);
-    });
+
+    // Only initialize Locomotive Scroll on larger screens or non-iOS devices
+    if (window.innerWidth > 768) {
+        const scroll = new LocomotiveScroll({
+            el: document.querySelector('[data-scroll-container]'),
+            smooth: true
+        });
+
+        scrollDownButton.addEventListener('click', function () {
+            scroll.scrollTo(bodySection, {
+                duration: 100,
+                offset: 0,
+                easing: [0.45, 0.05, 0.55, 0.95]
+            });
+        });
+    } else {
+        scrollDownButton.addEventListener('click', function () {
+            bodySection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
 });
